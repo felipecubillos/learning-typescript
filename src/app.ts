65077b43839ca291e0674cc2f9afb68656b1fec8
Felipe Cubillos <felipe.cubillos@outlook.com>
2021-03-17 23:08:41 -0500

    learning about types ...nothing new :(

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+console.log("inicia el camino!!");
+
+console.log(" Working with core types");
+
+function add(number1:number, number2: number) : number {
+    console.log("conociendo el tipo de dato de una variable: ", typeof(number1));
+
+    
+
+    try {
+        if (typeof "hola" === "string") {
+            throw new Error("nueva exepcion por x");
+        }
+    } catch (error) {
+        console.error("atrapando el error ",error);
+        
+    }
+    
+    return number1+number2;
+}
+
+console.log("la suma de los numeros : ",add(23,45));
